5241570347ee2ae727a68d9c9b699e3380413818
Vito <vitof970@gmail.com>
2018-12-29 22:34:43 +0100

    Project Initialized

---
+++ app/src/main/java/ferru97/dev/randomgooglebackgroundandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package ferru97.dev.randomgooglebackgroundandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
